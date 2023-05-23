212ca5e78a3ecba0831481380d97074d750c92db
jenyadev3 <112981213+jenyadev3@users.noreply.github.com>
2023-05-23 22:31:37 +0300

    Update

--- settings.js
+++ settings.js
@@ -1,9 +1,21 @@
 const TIMEOUT = 1000;
-window.TIMEOUT = TIMEOUT;
+
 
 const AppRoute = {
+    Initial: 'initial',
     Game: 'game',
     Results: 'results',
 }
 
+const GAME_TYPES = [
+    { type: 'cats', icon: 'cats', label: 'Котики' },
+    { type: 'flowers', icon: 'flowers', label: 'Цветочки' },
+    { type: 'cars', icon: 'cars', label: 'Машины' },
+  ];
+
+const RANDOMIZED = false;
+
 window.AppRoute = AppRoute;
+window.TIMEOUT = TIMEOUT;
+window.GAME_TYPES = GAME_TYPES;
+window.RANDOMIZED = RANDOMIZED;
